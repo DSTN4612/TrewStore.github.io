cb93b0406758c4fc43e008e74f84b43ae9f970f5
DSTN4612 <DroneSurveyTN@gmail.com>
2020-05-08 07:12:02 -0400

    Initial commit

---
+++ pointclouds/TrewStore/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 6891944,
+    "boundingBox": {
+        "lx": 717538.500516537,
+        "ly": 3906863.6212726475,
+        "lz": 160.483319335891,
+        "ux": 717641.909554962,
+        "uy": 3906967.0303110724,
+        "uz": 263.89235776084856
+    },
+    "tightBoundingBox": {
+        "lx": 717538.5,
+        "ly": 3906863.62,
+        "lz": 160.48,
+        "ux": 717639.98,
+        "uy": 3906967.03,
+        "uz": 179.22
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.8955485224723816,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
